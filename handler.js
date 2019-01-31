b3a8264fe8fa012345292c65148d7de690ea3780
rafael <you@example.com>
2019-01-31 21:45:40 -0200

    reitrar codigo comentado

--- handler.js
+++ handler.js
@@ -1,19 +1,11 @@
 "use strict";
 var mongoose = require("mongoose");
 const Process = require("./Model/Proces");
-mongoose.connect(
-    "mongodb://process:slack123@ds161794.mlab.com:61794/process",
-    { useNewUrlParser: true }
-);
+mongoose.connect("mongodb://process:slack123@ds161794.mlab.com:61794/process", {
+    useNewUrlParser: true
+});
 
 const insert = (params) => {
-    /* var slackDataSchema = new Schema({
-        mensage: { type: String, required: true },
-        channel: String
-    });
-
-    var Process = mongoose.model("SlackData", slackDataSchema); */
-
     const process = new Process({
         name: params.name,
         userId: params.userId,
